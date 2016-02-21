ff68d43fbe48de4afb67649c2feb01482f859cab
gravityfox <gravityreallyhatesme@gmail.com>
2016-02-21 13:08:00 -0800

    Starting selections...

---
+++ src/main/java/net/foxdenstudio/sponge/foxcore/plugin/state/SelectionStateField.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of FoxCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) gravityfox - https://gravityfox.net/
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.foxdenstudio.sponge.foxcore.plugin.state;
+
+import com.google.common.collect.ImmutableList;
+import net.foxdenstudio.sponge.foxcore.plugin.command.util.ProcessResult;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.text.Text;
+
+import java.util.List;
+
+/**
+ * Created by Fox on 2/19/2016.
+ * Project: SpongeForge
+ */
+public class SelectionStateField extends StateFieldBase {
+
+    protected SelectionStateField() {
+        super("Current Selection");
+    }
+
+    @Override
+    public Text currentState() {
+        return Text.of();
+    }
+
+    @Override
+    public ProcessResult modify(CommandSource source, String arguments) throws CommandException {
+        return ProcessResult.failure();
+    }
+
+    @Override
+    public List<String> modifySuggestions(CommandSource source, String arguments) throws CommandException {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+}
