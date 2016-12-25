69a65b4d2e418ee605d21ad6412f2445cf81c3f5
gravityfox <gravityreallyhatesme@gmail.com>
2016-12-24 21:46:30 -0800

    Added Who and What commands.

--- src/main/java/net/foxdenstudio/sponge/foxcore/plugin/command/misc/CommandPWD.java
+++ src/main/java/net/foxdenstudio/sponge/foxcore/plugin/command/misc/CommandPWD.java
@@ -7,6 +7,8 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
+import java.util.Optional;
+
 /**
  * Created by Fox on 11/5/2016.
  */
@@ -21,5 +23,21 @@ public class CommandPWD extends FCCommandBase {
     @Override
     public boolean testPermission(CommandSource source) {
         return source.hasPermission("foxcore.command.misc.pwd");
+    }
+
+    @Override
+    public Optional<Text> getShortDescription(CommandSource source) {
+        return Optional.of(Text.of("Prints the working directory of the game."));
+    }
+
+    @Override
+    public Optional<Text> getHelp(CommandSource source) {
+        return Optional.of(Text.of("This command prints the directory from which the game was started.\n" +
+                "On servers, this corresponds to the folder the server was started from, usually the folder with the server jar."));
+    }
+
+    @Override
+    public Text getUsage(CommandSource source) {
+        return Text.of("/foxcore misc pwd");
     }
 }
