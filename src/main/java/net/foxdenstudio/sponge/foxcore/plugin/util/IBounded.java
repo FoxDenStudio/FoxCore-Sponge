80356a4d2f12843a60319f889c843c3eb7a0dbd7
gravityfox <gravityreallyhatesme@gmail.com>
2017-06-05 16:06:05 -0700

    Bounding interfaces refactored to be more useful.

--- src/main/java/net/foxdenstudio/sponge/foxcore/plugin/util/IBounded.java
+++ src/main/java/net/foxdenstudio/sponge/foxcore/plugin/util/IBounded.java
@@ -2,6 +2,7 @@ package net.foxdenstudio.sponge.foxcore.plugin.util;
 
 import com.flowpowered.math.vector.Vector3d;
 import com.flowpowered.math.vector.Vector3i;
+import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 /**
@@ -15,10 +16,18 @@ public interface IBounded {
         return this.contains(vec.getX(), vec.getY(), vec.getZ(), world);
     }
 
+    default boolean containsBlock(Location<World> loc){
+        return this.contains(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), loc.getExtent());
+    }
+
     boolean contains(double x, double y, double z, World world);
 
     default boolean contains(Vector3d vec, World world) {
         return this.contains(vec.getX(), vec.getY(), vec.getZ(), world);
+    }
+
+    default boolean contains(Location<World> loc){
+        return this.contains(loc.getX(), loc.getY(), loc.getZ(), loc.getExtent());
     }
 
     default boolean isInChunk(Vector3i chunk, World world) {
